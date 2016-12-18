696a5a87a36e8c0c9e495beb0b9eb9b8eed0c10b
Syed Aziz. Enam <senam@SENAM-MBSF.local>
2016-12-17 17:46:25 -0800

    bugs

--- train_model.py
+++ train_model.py
@@ -64,6 +64,7 @@ for i in range(10):
         distorted_img[i,:,:,0] = distort_data(examples[i,:,:,0])
     X_train_gray = np.vstack((X_train_gray, distorted_img))
     y_train = np.vstack((y_train, labels))
+    print(X_train_gray.shape, y_train.shape)
 X_train_gray , X_dev_gray, y_train, y_dev = train_test_split(X_train_gray, y_train, test_size = 0.01, stratify = y_train)
 y_train, y_dev = preprocess_target(y_train), preprocess_target(y_dev)
 
